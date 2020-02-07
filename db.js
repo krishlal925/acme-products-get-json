d7f442d35d9031c872ecad55bc681e1c0d3542e0
Neel <krishlal925@gmail.com>
2020-02-06 19:33:50 -0800

    first commit

---
+++ db.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const readJSON = (file)=> {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(file, (err, data)=> {
+      if(data){
+        try {
+          resolve(JSON.parse(data.toString()));
+        }
+        catch(ex){
+          reject(ex);
+        }
+      }
+      else {
+        reject(err);
+      }
+    });
+  });
+};
+
+module.exports = {
+  readJSON
+};
