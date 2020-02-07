2cbf3f7430cea7d04708fd9ec410c4e39094f76d
Neel <krishlal925@gmail.com>
2020-02-06 20:15:26 -0800

    phase 1

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const db = require('./db')
 
 
 app.get('/', (req, res, next) =>{
   res.sendFile(path.join(__dirname, 'index.html'))
 });
 
+app.get('/api/products', async (req, res, next) =>{
+  const products = await db.readJSON('./products.json')
+  res.json(products)
+})
 
 app.listen(3000, ()=> console.log('listening on port 3000'))
